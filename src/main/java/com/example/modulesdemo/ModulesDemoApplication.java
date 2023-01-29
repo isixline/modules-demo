3c0dae36612f0f9749dc0210f7ca1d2cb1a80ae0
hang <isixline@qq.com>
2023-01-29 16:54:49 +0800

    feat: init

---
+++ src/main/java/com/example/modulesdemo/ModulesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulesDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modulesdemo/ModulesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
